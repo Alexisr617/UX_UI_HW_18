105028baa4d1ec42e0efebae655459b0ac14a501
Alexisr617 <66918651+Alexisr617@users.noreply.github.com>
2020-11-05 22:46:48 -0600

    Final

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Alexis Russell Profile");
+$("h1").css("color", "white");
+$(document).ready(function() {
+    $(".hello").addClass("opacity"); 
+    });
+    $(document).ready(function() {
+        $(".name").addClass("opacity"); 
+        });
